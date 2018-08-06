9afd91f86cebb4ff5f103b5061ba29698e143a51
Daniel Maynard <maynarda@oregonstate.edu>
2018-08-06 13:00:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://ds115022.mlab.com:15022/heroku_s5r22hdq/loginapp');
+mongoose.connect('mongodb://localhost/loginapp');
+
 
 
 var db = mongoose.connection;
