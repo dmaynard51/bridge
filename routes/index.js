71eda1559875930d6ec8eabeab5c54eac1e69b01
Reed Miller <reedwilliammiller@gmail.com>
2018-08-08 14:27:41 -0700

    Added the route for the account management page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
 
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
 	res.render('index');
+});
+
+
+// Get Homepage
+router.get('/account', ensureAuthenticated, function(req, res){
+	// Get User Info
+	User.findById(req.user.id, function (err, content) {
+		res.render('account', { title: 'Account Management', contents: content });
+	});
 });
 
 function ensureAuthenticated(req, res, next){
