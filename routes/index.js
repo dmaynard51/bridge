0b0f0f903c395728b504e59373f6fd31a3614153
Reed Miller <reedwilliammiller@gmail.com>
2018-08-09 14:09:13 -0700

    Added post route to update user info
    Update is not working yet.
    Need to figure out the mongodb update syntax to see if we are doing this correctly.
    Also need to implement the redirect to see the updated content after clicking submit.
    We can add a note on the front end letting the user know their info was updated also.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
@@ -10,13 +12,26 @@ router.get('/', ensureAuthenticated, function(req, res){
 });
 
 
-// Get Homepage
+// Get Account Management Page
 router.get('/account', ensureAuthenticated, function(req, res){
 	// Get User Info
 	User.findById(req.user.id, function (err, content) {
 		res.render('account', { title: 'Account Management', contents: content });
 	});
 });
+
+//post update
+router.post('/account', (req, res, next) => {
+    	//update the fields in the database for logged in user
+        db.collection("users").update({_id: req.user.id}, {$set:{'email': req.body.email, 'zip': req.body.zip}}, (err, result) => {
+          if(err) {
+            throw err;
+          }
+	//reload account management page (had issues with loading the db values again, probably because this is a post call, not a get)
+          res.render('account');
+          console.log('account info updated sucessfully');
+        });
+    });
 
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
